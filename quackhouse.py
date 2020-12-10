d9c1d1664302e6eabcbe23c7119d8b321000a1d6
Andy Dyckman <adyckman@inoc.com>
2020-12-09 19:23:45 -0600

    cleanup

--- quackhouse.py
+++ quackhouse.py
@@ -1,13 +1,14 @@
 import datetime, time, ephem, math, threading, logging, yaml, argparse
 from gpiozero import Motor
 
-# Take in initial door status from commandline
-import argparse
+# Take in initial door status and test option from commandline
 parser = argparse.ArgumentParser()
-parser.add_argument('--state', type=str, required=True, help='Current state of the door.  Either Opened or Closed')
+parser.add_argument('--state', type=str, required=True, help='Current state of the door.  Either Open or Closed')
+parser.add_argument('--test', action='store_true', 
+    help="Runs in test mode, without invoking the Rasperry Pi pins")
 args = parser.parse_args()
-if ((args.state != 'Opened') and (args.state != 'Closed')):
-    print('You specified {0}, not "Opened" or "Closed" - try again stupid'.format(args.state))
+if ((args.state != 'Open') and (args.state != 'Closed')):
+    print('You specified {0}, not "Open" or "Closed" - try again stupid'.format(args.state))
     quit()
 else:
     door_status = args.state
@@ -17,8 +18,8 @@ with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 # Set up the linear actuator's motor and what pins on the Pi triggers the relays needed for each direction.  forward opens the door, backward closes it.
-motor = Motor(forward=cfg['pi']['forward_pin'], backward=cfg['pi']['backward_pin'])
-
+if not args.test:
+    motor = Motor(forward=cfg['pi']['forward_pin'], backward=cfg['pi']['backward_pin'])
 
 # Configure Logging
 logging.basicConfig(level=cfg['logging']['level'],
@@ -31,10 +32,10 @@ def sun_altitude():
     # Instantanize the Sun as the object to reference
     sun = ephem.Sun()
     observer = ephem.Observer()
-    #  Observer time and locaion settings here.  Based on Chicagoland, IL
-    observer.lat = cfg['location']['latitude'] # Latitude
-    observer.lon = cfg['location']['longitude'] # Longitude
-    observer.elevation = cfg['location']['elevation'] # Elevation in Meters
+    #  Observer time and locaion settings here.
+    observer.lat = cfg['location']['latitude']
+    observer.lon = cfg['location']['longitude']
+    observer.elevation = cfg['location']['elevation']
     #  Set the current time (in UTC) here
     observer.date = datetime.datetime.utcnow()
     # Computes the position of the Sun from the time/location specified above
@@ -45,22 +46,28 @@ def sun_altitude():
 # Open ze door
 def open_door():
     global door_status
+    logging.info('Initializing Door Opening')
     door_status = 'Opening'
     logging.info(door_status)
-    motor.forward()
+    if not args.test:
+        motor.forward()
     time.sleep(cfg['door']['duration'])
-    motor.stop()
+    if not args.test:
+        motor.stop()
     door_status = 'Open'
     logging.info(door_status)
 
 # Close the door
 def close_door():
     global door_status
+    logging.info('Initializing Door Closing')
     door_status = 'Closing'
     logging.info(door_status)
-    motor.backward()
+    if not args.test:
+        motor.backward()
     time.sleep(cfg['door']['duration'])
-    motor.stop()
+    if not args.test:
+        motor.stop()
     door_status = 'Closed'
     logging.info(door_status)
 
@@ -69,18 +76,18 @@ open_the_door = threading.Thread(target=open_door)
 close_the_door = threading.Thread(target=close_door)
 
 # Horrible printing of initial logging settings
-logging.info('Current Time: {0}, Initial Door Status: {1}'.format(datetime.datetime.utcnow()), door_status)
+logging.info("Current Time: {time}, Initial Door Status: {status}, Test: {test}".format(time=datetime.datetime.utcnow(), status=door_status, test=str(args.test)))
 logging.info('LOCATION SETTINGS')
-logging.info("Latitude: {0}, Longitude: {1}, Elevation: {2}".format(cfg['location']['latitude'], cfg['location']['longitude'], cfg['location']['elevation']))
+logging.info("Latitude: {lat}, Longitude: {lon}, Elevation: {elev}".format(lat=cfg['location']['latitude'], lon=cfg['location']['longitude'], elev=cfg['location']['elevation']))
 logging.info('')
 logging.info('LOGGING SETTINGS')
-logging.info("Level: {0}, Filename: {1}, Format: {2}".format(cfg['logging']['level'], cfg['logging']['filename'], cfg['logging']['format']))
+logging.info("Level: {lev}, Filename: {file}, Format: {format}".format(lev=cfg['logging']['level'], file=cfg['logging']['filename'], format=cfg['logging']['format']))
 logging.info('')
 logging.info('PI SETTINGS')
-logging.info("Forward Pin: {0}, Backward Pin: {1}".format(cfg['pi']['forward_pin'], cfg['pi']['backward_pin']))
+logging.info("Forward Pin: {fwd}, Backward Pin: {back}".format(fwd=cfg['pi']['forward_pin'], back=cfg['pi']['backward_pin']))
 logging.info('')
 logging.info('DOOR SETTINGS')
-logging.info("Duration: {0}, Open Elevation: {1}, Close Elevation: {2}".format(cfg['door']['duration'], cfg['door']['open_elevation'], cfg['door']['close_elevation']))
+logging.info("Duration: {dur}, Open Elevation: {open}, Close Elevation: {close}".format(dur=cfg['door']['duration'], open=cfg['door']['open_elevation'], close=cfg['door']['close_elevation']))
 logging.info('')
 
 def main():
